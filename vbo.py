c9517d03502e867c19d8e5dfa88ed93300602d11
David Sveningsson <ext@sidvind.com>
2012-07-13 16:09:31 +0200

    use ctype

--- vbo.py
+++ vbo.py
@@ -1,5 +1,6 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from ctypes import c_void_p
 import numpy as np
 
 class VBO(object):
@@ -27,7 +28,7 @@ class VBO(object):
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.buffer[1])
 
         #glVertexPointer(3, GL_FLOAT, self.stride, None)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, self.stride, None)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, self.stride, c_void_p(0))
         #glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(vertex_t), (const GLvoid*) (sizeof(glm::vec3)))
         #glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_t), (const GLvoid*) (sizeof(glm::vec3)+sizeof(glm::vec2)))
         #glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_t), (const GLvoid*) (2*sizeof(glm::vec3)+sizeof(glm::vec2)))
